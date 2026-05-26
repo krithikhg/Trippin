1bb7e86e82d1f77cc984ef5999289d6ad100a67f
Jolyn <jolyn8136@gmail.com>
2026-05-27 00:47:40 +0800

    Add Supabase client setup

---
+++ utils/supabase/client.ts
@@ -0,0 +1,10 @@
+//reusable helper that lets any browser-side code in Trippin to connect to Supabase
+
+import { createBrowserClient } from '@supabase/ssr'
+
+export function createClient() {
+  return createBrowserClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY!
+  )
+}
