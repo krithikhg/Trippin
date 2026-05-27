f49452015ff4b87d63f7cfd54a84b93c27e03962
Jolyn <jolyn8136@gmail.com>
2026-05-27 19:49:54 +0800

    Before shadcn setup

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -4,7 +4,7 @@ export default function LoginPage() {
   return (
     <div className="flex flex-1 items-center justify-center p-8">
       <form className="flex flex-col gap-4 w-full max-w-sm">
-        <h1 className="text-6xl font-serif italic text-heading text-center tracking-tight">Welcome to Trippin! </h1>
+        <h1 className="text-7xl font-serif italic text-heading text-center tracking-tight">Welcome to Trippin! </h1>
 
         <label htmlFor="email">Email:</label>
         <input
